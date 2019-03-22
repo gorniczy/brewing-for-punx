329cf83705c7bf670ba359efbc584770d354042f
gorniczy <denysgornichar@gmail.com>
2019-03-22 19:46:37 +0100

    delete unnecessary componentDidMount()

--- src/App.js
+++ src/App.js
@@ -15,11 +15,8 @@ class App extends React.Component {
 
   componentDidMount() {
     this.props.fetchBeers("https://api.punkapi.com/v2/beers?page=1&per_page=80");
+    console.log(this.props.error);
   };
-
-  componentDidUpdate() {
-
-  }
 
 
   render() {
