330be1be57580219c61582378c2cb10d2b68111b
gorniczy <denysgornichar@gmail.com>
2019-04-21 23:17:50 +0200

    Updates

---
+++ precache-manifest.17b00c3684ad851180063d00c2cd1b6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9505d40565b48fcb1541119d31045668",
+    "url": "/brewing-for-punx/static/media/logo.9505d405.png"
+  },
+  {
+    "revision": "2cff134c9acb64d78c5afe90728ecc59",
+    "url": "/brewing-for-punx/static/media/exit.2cff134c.svg"
+  },
+  {
+    "revision": "6690aa17af6888fd7822",
+    "url": "/brewing-for-punx/static/js/runtime~main.f66320ed.js"
+  },
+  {
+    "revision": "25da7b9c3b80791abb6d",
+    "url": "/brewing-for-punx/static/js/main.3ecf5a15.chunk.js"
+  },
+  {
+    "revision": "00d868a755be1ad6d13f",
+    "url": "/brewing-for-punx/static/js/2.075e8d41.chunk.js"
+  },
+  {
+    "revision": "25da7b9c3b80791abb6d",
+    "url": "/brewing-for-punx/static/css/main.c2e69067.chunk.css"
+  },
+  {
+    "revision": "9a00eb5048963ea4548754edf3802924",
+    "url": "/brewing-for-punx/index.html"
+  }
+];
